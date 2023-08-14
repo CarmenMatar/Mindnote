eb991688006fdb8c576880862ae3781e0ed2b01c
CarmenMatar <85544723+CarmenMatar@users.noreply.github.com>
2023-08-14 15:37:06 +0300

    Create new components

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login">
+        <h1>Login</h1>
+    </div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      title: 'Login'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainDashboard from '@/components/MainDashboard'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'mainDash',
       component: MainDashboard
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
