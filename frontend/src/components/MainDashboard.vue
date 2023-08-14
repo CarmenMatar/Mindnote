bcf10e6210392af4b051c415dab0e8cf3b86a4fb
CarmenMatar <85544723+CarmenMatar@users.noreply.github.com>
2023-08-14 15:05:29 +0300

    New Frontend project

---
+++ frontend/src/components/MainDashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mainDash">
+    <h1>Main Dashboard</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainDashboard',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
