c4877611f55d99a74af36e85760309c681d88ebd
CarmenMatar <85544723+CarmenMatar@users.noreply.github.com>
2023-09-04 22:21:24 +0300

    Add bootstrap

--- frontend/src/General/Views/index.vue
+++ frontend/src/General/Views/index.vue
@@ -1,15 +1,32 @@
 <template>
  <div class="index">
-  <h1>Landing Page</h1>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Mindnote</b-navbar-brand>
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Login</b-button>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Sign Up</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
  </div>
 </template>
 <script>
+import LoginVue from '../components/Login.vue'
 export default {
   name: 'index',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    LoginVue
   }
 }
 </script>
